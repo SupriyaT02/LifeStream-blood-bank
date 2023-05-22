d9e02cc0c119c4aa9213b8906cca4014b09be34a
SupriyaT02 <115412070+SupriyaT02@users.noreply.github.com>
2023-05-22 23:13:21 +0530

    Add files via upload

---
+++ blood/ticker.php
@@ -0,0 +1,61 @@
+
+
+<html>
+<head>
+<style>
+@media screen and (max-width: 80px) {
+  .news {
+    position:relative;margin-left:auto;margin-right:auto;margin-top:400px  ;
+  }
+  .text1{
+  	box-shadow:none !important;
+    position:relative;
+      margin-left:auto;margin-right:auto;
+  }
+}
+.blue {
+    background: #347fd0;
+}
+
+.news {
+    box-shadow: inset 0 -15px 30px rgba(10,4,60,0.4), 0 5px 10px rgba(10,20,100,0.5);
+       width: 100%;
+       height:40px;
+    margin-top:0px;
+    overflow: hidden;
+
+    border-radius: 4px;
+    padding: 1px;
+    /*-webkit-user-select: none;*/
+
+}
+
+
+.news span {
+    float: left;
+    color: #fff;
+    padding: 5px;
+    position: relative;
+    top: 1%;
+    box-shadow: inset 0 -15px 30px rgba(0,0,0,0.4);
+    font: 16px 'Raleway', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    /*-webkit-user-select: none;*/
+    /*cursor: pointer;*/
+}
+
+.text1{
+
+	box-shadow:none !important;
+  position:relative;
+    width:90%
+}
+</style>
+</head>
+<body>
+<div class="news blue">
+<span style="background-color:#F60F0F;width:125px;height:40px">Latest Updates</span><span class="text1" ><marquee>A blood donation camp to be organised at Chennai Institute of Technology in collaboration with LifeStream blood bank on <b>29/04/2023</b> at <b>Parthasarathy auditorium,CIT</b>. Save a life with a unit of blood.</marquee></span>
+</div>
+</body>
+</head>
+</html>
